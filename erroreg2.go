c34277a4ee9f4d7304b87683976bb0042bab1e9b
RamDev <daudalaramadan2@gmail.com>
2025-11-03 22:14:28 +0000

    Refactor square function to improve error handling for negative input

--- erroreg2.go
+++ erroreg2.go
@@ -1,35 +1,61 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-// CustomError is a custom error type
-type CustomError struct {
-	Code    int
-	Message string
-}
+// reusable error in Go
 
-// Error implements the error interface
-func (e *CustomError) Error() string {
-	return fmt.Sprintf("Error %d: %s", e.Code, e.Message)
-}
-
-func divide(a, b float64) (float64, error) {
-	if b == 0 {
-		return 0, &CustomError{
-			Code:    400,
-			Message: "division by zero is not allowed",
-		}
+func square(x int) (int, error) {
+	if x < 0 {
+		return 0, fmt.Errorf("the int isnt a positive number %v", x)
 	}
-	return a / b, nil
-}
 
+	return x * x, nil
+
+}
 func main() {
-	result, err := divide(10, 0)
+
+	result, err := square(-3)
 	if err != nil {
-		fmt.Println("Error occurred:", err)
-		return
+		fmt.Println(err)
+
+	} else {
+		fmt.Println("result:", result)
 	}
-	fmt.Printf("Result: %.2f\n", result)
+
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// // CustomError is a custom error type
+// type CustomError struct {
+// 	Code    int
+// 	Message string
+// }
+
+// // Error implements the error interface
+// func (e *CustomError) Error() string {
+// 	return fmt.Sprintf("Error %d: %s", e.Code, e.Message)
+// }
+
+// func divide(a, b float64) (float64, error) {
+// 	if b == 0 {
+// 		return 0, &CustomError{
+// 			Code:    400,
+// 			Message: "division by zero is not allowed",
+// 		}
+// 	}
+// 	return a / b, nil
+// }
+
+// func main() {
+// 	result, err := divide(10, 0)
+// 	if err != nil {
+// 		fmt.Println("Error occurred:", err)
+// 		return
+// 	}
+// 	fmt.Printf("Result: %.2f\n", result)
+// }
