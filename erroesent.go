18bd438868109a0c089a757e3066af40e901e85c
RamDev <daudalaramadan2@gmail.com>
2025-11-04 01:51:33 +0000

    Implement BreadInStock function with error handling for out-of-stock scenario

--- erroesent.go
+++ erroesent.go
@@ -1,5 +1,24 @@
 package main
 
+import "errors"
+
+// the sentinal Error
+
+var ErrNoBread = errors.New("No Bread Left In Stock")
+
+func BreadInStock(stock int) (string, error) {
+	if stock == 0 {
+		return "", ErrNoBread
+
+	}
+	return "Here’s your bread!", nil
+
+}
+
 func main() {
+	result, err := BreadInStock(0)
+	if err != nil {
+
+	}
 
 }
